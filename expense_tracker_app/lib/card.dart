4d41a523da73455002f1b6f9e26d8a9a2d9c6f3d
reuben21 <reuben211999@icloud.com>
2021-03-15 22:40:32 +0530

    Updated UI

--- expense_tracker_app/lib/card.dart
+++ expense_tracker_app/lib/card.dart
@@ -2,45 +2,69 @@ import 'package:flutter/material.dart';
 
 class CardWidget extends StatelessWidget {
   String title;
-  double amount;
+  String amount;
+  String date;
 
-  CardWidget(this.title,this.amount) {}
+  CardWidget(this.title,this.amount,this.date) {}
 
   @override
   Widget build(BuildContext context) {
     return Container(
       width: double.infinity,
       height: 100,
-
       child: Card(
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
         color:  Colors.blueAccent,
         child: Row(
           children: <Widget>[
             Expanded(
-              flex: 2,
-              child:
-                  Text(
-                      title,
-                      textAlign: TextAlign.center,
-                      style: TextStyle(fontSize: 20)
-                  ),
-            ),
-            Expanded(
               flex: 1,
               child:
               Text(
-                  "\$"+amount.toString(),
+                  amount,
                   textAlign: TextAlign.center,
-                  style: TextStyle(fontSize: 20)
+                  style: TextStyle(fontSize: 18)
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child:
+              Container(
+
+                child: Padding(
+                  padding: const EdgeInsets.all(5.0),
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    crossAxisAlignment: CrossAxisAlignment.center,
+                    children: <Widget>[
+                      Text(
+                          title,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 16)
+                      ),
+                      SizedBox(height: 10),
+                      Text(date,
+                          textAlign: TextAlign.center,
+                          style: TextStyle(fontSize: 16)
+                      ),
+                    ],
+                  ),
+                ),
               ),
             ),
             Expanded(
               flex: 1,
-              child: Icon(
-                Icons.delete_outline,
+              child: IconButton(
+                icon: const Icon(
+                  Icons.delete_outline,
+                  color: Colors.white,
+                  size: 25.0,
+                  semanticLabel: 'Delete Icon',
+                ),
                 color: Colors.white,
-                size: 25.0,
-                semanticLabel: 'Delete Icon',
+                onPressed: () {},
               ),
             )
           ],
--- expense_tracker_app/lib/main.dart
+++ expense_tracker_app/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import './transaction.dart';
 import './card.dart';
 
@@ -17,12 +18,13 @@ class MyApp extends StatelessWidget {
 class MyHomePage extends StatelessWidget {
   final List<Transaction> transactions = [
     Transaction(
-        id: 't1', title: 'A New Monitor', amount: 69.99, date: DateTime.now()
-    ),
+        id: 't1', title: 'A New Monitor', amount: 69.99, date: DateTime.now()),
     Transaction(
-        id: 't1', title: 'A New Mouse', amount: 32.99, date: DateTime.now()
-    )
+        id: 't1', title: 'A New Mouse', amount: 32.99, date: DateTime.now())
   ];
+
+  final titleController = TextEditingController();
+  final amountController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
@@ -31,12 +33,84 @@ class MyHomePage extends StatelessWidget {
           title: Text('Personal Expense Tracker'),
         ),
         body: Column(
-          mainAxisAlignment: MainAxisAlignment.end,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: transactions.map((tx){
-            return CardWidget(tx.title,tx.amount);
-          }).toList()
-          ,
+          children: <Widget>[
+            Container(
+              child: Column(
+                  mainAxisAlignment: MainAxisAlignment.end,
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: TextFormField(
+                        controller: titleController,
+                        cursorColor: Colors.blueAccent,
+                        maxLength: 20,
+                        decoration: InputDecoration(
+                          labelText: 'Title',
+                          labelStyle: TextStyle(
+                            color: Colors.blueAccent,
+                            fontSize: 20,
+                          ),
+                          // helperText: 'Amount',
+                          suffixIcon: Icon(
+                            Icons.check_circle,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF6200EE)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(12.0),
+                      child: TextFormField(
+                        controller: amountController,
+                        cursorColor: Colors.blueAccent,
+                        maxLength: 20,
+                        decoration: InputDecoration(
+                          labelText: 'Amount',
+                          labelStyle: TextStyle(
+                            color: Colors.blueAccent,
+                            fontSize: 20,
+                          ),
+                          // helperText: 'Helper text',
+                          suffixIcon: Icon(
+                            Icons.check_circle,
+                          ),
+                          enabledBorder: UnderlineInputBorder(
+                            borderSide: BorderSide(color: Color(0xFF6200EE)),
+                          ),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: OutlinedButton.icon(
+                        onPressed: () {
+                          print(titleController.text);
+                          // Respond to button press
+                        },
+                        icon: Icon(Icons.add, size: 18),
+                        label: Text("Add Item",
+                            style: TextStyle(
+                              color: Colors.blueAccent,
+                              fontSize: 18,
+                            )),
+                      ),
+                    )
+                  ]),
+            ),
+            Container(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.end,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: transactions.map((tx) {
+                  return CardWidget(tx.title, '\$${tx.amount.toString()}',
+                      DateFormat.yMMMd().format(tx.date));
+                }).toList(),
+              ),
+            ),
+          ],
         ));
   }
 }
