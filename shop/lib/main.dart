c8acecaf15396df977e9145b04fc26d1ddd32f5d
reuben21 <reuben211999@icloud.com>
2021-04-08 11:41:56 +0530

    Updated Styles and Theme

--- shop/lib/main.dart
+++ shop/lib/main.dart
@@ -1,85 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:shop/screens/products_overview_screen.dart';
-
+import './colors.dart';
 void main() {
   runApp(MyApp());
 }
 
-const MaterialColor kPrimaryColor = const MaterialColor(
-  0xFF0E7AC7,
-  const <int, Color>{
-    50: const Color(0xFF2f5d62),
-    100: const Color(0xFF2f5d62),
-    200: const Color(0xFF2f5d62),
-    300: const Color(0xFF2f5d62),
-    400: const Color(0xFF2f5d62),
-    500: const Color(0xFF2f5d62),
-    600: const Color(0xFF2f5d62),
-    700: const Color(0xFF2f5d62),
-    800: const Color(0xFF2f5d62),
-    900: const Color(0xFF2f5d62),
-  },
-);
-
-
-const MaterialColor kPrimaryColorAccent = const MaterialColor(
-  0xFF0E7AC7,
-  const <int, Color>{
-    50: const Color(0xFF364547),
-    100: const Color(0xFF364547),
-    200: const Color(0xFF364547),
-    300: const Color(0xFF364547),
-    400: const Color(0xFF364547),
-    500: const Color(0xFF364547),
-    600: const Color(0xFF364547),
-    700: const Color(0xFF364547),
-    800: const Color(0xFF364547),
-    900: const Color(0xFF364547),
-  },
-);
 
 
 
 
-const MaterialColor kSecondaryColor = const MaterialColor(
-  0xFF0E7AC7,
-  const <int, Color>{
-    50: const Color(0xFFffb037),
-    100: const Color(0xFFffb037),
-    200: const Color(0xFFffb037),
-    300: const Color(0xFFffb037),
-    400: const Color(0xFFffb037),
-    500: const Color(0xFFffb037),
-    600: const Color(0xFFffb037),
-    700: const Color(0xFFffb037),
-    800: const Color(0xFFffb037),
-    900: const Color(0xFFffb037),
-  },
-);
-
-
-const MaterialColor kSecondaryColorAccent = const MaterialColor(
-  0xFF0E7AC7,
-  const <int, Color>{
-    50: const Color(0xFFffe268),
-    100: const Color(0xFFffe268),
-    200: const Color(0xFFffe268),
-    300: const Color(0xFFffe268),
-    400: const Color(0xFFffe268),
-    500: const Color(0xFFffe268),
-    600: const Color(0xFFffe268),
-    700: const Color(0xFFffe268),
-    800: const Color(0xFFffe268),
-    900: const Color(0xFFffe268),
-  },
-);
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Shopyify',
+      title: '',
       theme: ThemeData(
         // This is the theme of your application.
         //
