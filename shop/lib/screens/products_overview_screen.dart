468f97310253ab7973d5f2cf0446f464f0da2ab7
reuben21 <reuben211999@icloud.com>
2021-04-19 19:28:17 +0530

    Using Nested Models & Providers

--- shop/lib/screens/products_overview_screen.dart
+++ shop/lib/screens/products_overview_screen.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 import '../widgets/product_grid.dart';
-import '../models/product.dart';
+import '../providers/product.dart';
 
 class ProductsOverviewScreen extends StatelessWidget {
   final List<Product> loadedProducts = [
