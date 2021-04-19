cd5c9ec3c0a0387c9eadc14fb8015970b78469da
reuben21 <reuben211999@icloud.com>
2021-04-19 19:28:17 +0530

    Using Nested Models & Providers

--- lib/widgets/product_grid.dart
+++ lib/widgets/product_grid.dart
@@ -8,19 +8,23 @@ class ProductsGrid extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-
     final productsData = Provider.of<Products>(context);
     final products = productsData.items;
 
-
     // TODO: implement build
-    return GridView.builder(padding: const EdgeInsets.all(10.0),
+    return GridView.builder(
+      padding: const EdgeInsets.all(10.0),
       gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
           crossAxisCount: 2,
           childAspectRatio: 3 / 2,
           crossAxisSpacing: 10,
-          mainAxisSpacing: 10)
-      , itemBuilder: (ctx, i) => ProductItem(products[i].id,products[i].title,products[i].imageUrl)
-      ,itemCount: products.length,);
+          mainAxisSpacing: 10),
+      itemBuilder: (ctx, i) => ChangeNotifierProvider(
+        create: (c)=> products[i],
+          child: ProductItem(),
+
+      ),
+      itemCount: products.length,
+    );
   }
 }
--- lib/widgets/product_item.dart
+++ lib/widgets/product_item.dart
@@ -1,63 +1,68 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:shop/providers/product.dart';
 import '../colors.dart';
 import '../screens/product_detail_screen.dart';
 
 class ProductItem extends StatelessWidget {
-  final String id;
-  final String title;
-  final String imageUrl;
-
-  ProductItem(this.id, this.title, this.imageUrl);
+  // final String id;
+  // final String title;
+  // final String imageUrl;
+  //
+  // ProductItem(this.id, this.title, this.imageUrl);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ClipRRect(
+    final product = Provider.of<Product>(context);
 
+    return ClipRRect(
         borderRadius: BorderRadius.circular(12),
         child: GridTile(
           child: GestureDetector(
-            onTap: (){
-              Navigator.of(context).pushNamed(ProductDetailScreen.routeName, arguments: id);
+            onTap: () {
+              Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
+                  arguments: product.id);
             },
             child: Image.network(
-              imageUrl,
+              product.imageUrl,
               fit: BoxFit.cover,
             ),
           ),
           footer: Container(
             color: kPrimaryColorAccent[100],
-            child: Row(
-                children: <Widget>[
-                  Expanded(
-                    child: IconButton(
-                      icon: Icon(
-                        Icons.favorite, color: kSecondaryColor[100], size: 18,),
-                      onPressed: () {},
-                    ),
+            child: Row(children: <Widget>[
+              Expanded(
+                child: IconButton(
+                  icon: Icon(
+                    product.isFavorite ? Icons.favorite : Icons.favorite_border,
+                    color: kSecondaryColor[100],
+                    size: 18,
                   ),
-                  Expanded(
-                      child: Text(
-                        title,
-                        textAlign: TextAlign.center,
-                        style: TextStyle(
-                            color: kSecondaryColor[100], fontSize: 14),),
+                  onPressed: () {
+                    product.toggleFavoriteStatus();
+                  },
+                ),
+              ),
+              Expanded(
+                child: Text(
+                  product.title,
+                  textAlign: TextAlign.center,
+                  style: TextStyle(color: kSecondaryColor[100], fontSize: 14),
+                ),
+              ),
+              Expanded(
+                child: IconButton(
+                  icon: Icon(
+                    Icons.shopping_cart,
+                    color: kSecondaryColor[100],
+                    size: 18,
                   ),
-                  Expanded(
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.shopping_cart, color: kSecondaryColor[100],
-                          size: 18,),
-                        onPressed: () {},
-                      ),
-                  ),
-
-
-                ]
-            ),
-
-
+                  onPressed: () {},
+                ),
+              ),
+            ]),
           ),
         ));
   }
