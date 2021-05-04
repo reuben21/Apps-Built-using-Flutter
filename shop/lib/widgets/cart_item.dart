0f28ed33adbdfbee43dabaa0a6998c0687ed85dd
reuben21 <reuben211999@icloud.com>
2021-05-04 09:38:46 +0530

    Making Cart Items Dismissible

--- shop/lib/widgets/cart_item.dart
+++ shop/lib/widgets/cart_item.dart
@@ -1,30 +1,55 @@
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 import 'package:shop/colors.dart';
+import 'package:shop/providers/cart.dart';
 
 class CartItem extends StatelessWidget {
   final String id;
+  final String productId;
   final double price;
   final int quantity;
   final String title;
 
-  CartItem(this.id, this.price, this.quantity, this.title);
+  CartItem(this.id,this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
-    return Card(
-      margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
-      child: Padding(
-        padding: EdgeInsets.all(8),
-        child: ListTile(
-          leading: CircleAvatar(
-            backgroundColor: kPrimaryColor[100],
-            child: FittedBox(child: Text('\$ ${price}')),
+    return Dismissible(
+      key:ValueKey(id),
+      direction: DismissDirection.endToStart,
+      onDismissed: (direction){
+          Provider.of<Cart>(context,listen: false).removeItem(productId);
+      },
+      background: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+
+          child: Icon(Icons.delete,color: Colors.white,size:40),
+          alignment: Alignment.centerRight,
+          padding: EdgeInsets.only(right:20 ),
+          decoration: BoxDecoration(
+              color:Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(5))
           ),
-          title: Text(title),
-          subtitle: Text('Total: \$${(price*quantity)}'),
-          trailing: Text('${quantity} x',style: Theme.of(context).textTheme.headline4),
+        ),
+      ),
+
+      child: Card(
+        margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
+        child: Padding(
+          padding: EdgeInsets.all(8),
+          child: ListTile(
+            leading: CircleAvatar(
+              radius: 30.0,
+              backgroundColor: kPrimaryColor[100],
+              child: FittedBox(child: Text('\$ ${price}',style: Theme.of(context).textTheme.headline4 ,)),
+            ),
+            title: Text(title),
+            subtitle: Text('Total: \$${(price*quantity)}'),
+            trailing: Text('${quantity} x',style: Theme.of(context).textTheme.headline4),
+          ),
         ),
       ),
     );
