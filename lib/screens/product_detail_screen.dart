cd71a7d19b36e19aa6697f2c3e8686ed15627156
reuben21 <reuben211999@icloud.com>
2021-05-04 11:09:02 +0530

    Adding Product Detail Data

--- lib/screens/product_detail_screen.dart
+++ lib/screens/product_detail_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:shop/colors.dart';
 import '../providers/products.dart';
 import 'package:provider/provider.dart';
 
@@ -9,18 +11,53 @@ class ProductDetailScreen extends StatelessWidget {
 
   static const routeName = '/product-detail';
 
-
-
   @override
   Widget build(BuildContext context) {
     final productId = ModalRoute.of(context).settings.arguments as String;
     // TODO: implement build
-    final loadedProduct = Provider.of<Products>(context,listen: false).findById(productId);
+    final loadedProduct =
+        Provider.of<Products>(context, listen: false).findById(productId);
 
-
-
-    return Scaffold(appBar: AppBar(
-      title: Text(loadedProduct.title, style: Theme.of(context).textTheme.headline1),
-    ),);
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(loadedProduct.title,
+            style: Theme.of(context).textTheme.headline1),
+      ),
+      body: Container(
+        height: double.infinity,
+        color: kPrimaryColorAccent[100],
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Container(
+                  height: 300,
+                  width: double.infinity,
+                  child:
+                      Image.network(loadedProduct.imageUrl, fit: BoxFit.cover),
+                  decoration: BoxDecoration(
+                      borderRadius: BorderRadius.all(Radius.circular(10))),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Price \$ ${loadedProduct.price}",
+                  style: Theme.of(context).textTheme.headline2,
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  loadedProduct.description,
+                  style: Theme.of(context).textTheme.headline4,
+                )
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
