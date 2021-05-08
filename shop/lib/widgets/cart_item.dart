80dbd5ca20e5613e05cdee6845e6bbb0295b5e87
reuben21 <reuben211999@icloud.com>
2021-05-08 07:59:50 +0530

    showing alert dialog box

--- shop/lib/widgets/cart_item.dart
+++ shop/lib/widgets/cart_item.dart
@@ -21,18 +21,19 @@ class CartItem extends StatelessWidget {
         return showDialog(
             context: context,
             builder: (ctx) => AlertDialog(
+              backgroundColor: kPrimaryColor[100],
                   title: Text(
-                    'Are you sure?',
+                    'Are you sure?',style: Theme.of(context).textTheme.headline1
                   ),
                   content:
-                      Text('Do you want to remove this item from the cart'),
+                      Text('Do you want to remove ${title} from the cart',style: Theme.of(context).textTheme.headline4),
                   actions: <Widget>[
-                    FlatButton(onPressed: (){
+                    TextButton(onPressed: (){
                       Navigator.of(ctx).pop(false);
-                    }, child: Text('No')),
-                    FlatButton(onPressed: (){
+                    }, child: Text('No',style: Theme.of(context).textTheme.headline3)),
+                    TextButton(onPressed: (){
                       Navigator.of(ctx).pop(true);
-                    }, child: Text('Yes'))
+                    }, child: Text('Yes',style: Theme.of(context).textTheme.headline3))
                   ],
                 ));
       },
@@ -53,21 +54,22 @@ class CartItem extends StatelessWidget {
         ),
       ),
       child: Card(
+        color: kPrimaryColor[100],
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         child: Padding(
           padding: EdgeInsets.all(8),
           child: ListTile(
             leading: CircleAvatar(
               radius: 30.0,
-              backgroundColor: kPrimaryColor[100],
+              backgroundColor: kPrimaryColorAccent[100],
               child: FittedBox(
                   child: Text(
                 '\$ ${price}',
                 style: Theme.of(context).textTheme.headline4,
               )),
             ),
-            title: Text(title),
-            subtitle: Text('Total: \$${(price * quantity)}'),
+            title: Text(title, style: Theme.of(context).textTheme.headline4,),
+            subtitle: Text('Total: \$${(price * quantity)}', style: Theme.of(context).textTheme.headline4,),
             trailing: Text('${quantity} x',
                 style: Theme.of(context).textTheme.headline4),
           ),
