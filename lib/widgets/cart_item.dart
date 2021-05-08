b51e3c052c0f4daabfe4dd939351a446b344c889
reuben21 <reuben211999@icloud.com>
2021-05-08 07:52:10 +0530

    showing alert dialog box

--- lib/widgets/cart_item.dart
+++ lib/widgets/cart_item.dart
@@ -10,32 +10,48 @@ class CartItem extends StatelessWidget {
   final int quantity;
   final String title;
 
-  CartItem(this.id,this.productId, this.price, this.quantity, this.title);
+  CartItem(this.id, this.productId, this.price, this.quantity, this.title);
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
 
     return Dismissible(
-      key:ValueKey(id),
+      confirmDismiss: (direction) {
+        return showDialog(
+            context: context,
+            builder: (ctx) => AlertDialog(
+                  title: Text(
+                    'Are you sure?',
+                  ),
+                  content:
+                      Text('Do you want to remove this item from the cart'),
+                  actions: <Widget>[
+                    FlatButton(onPressed: (){
+                      Navigator.of(ctx).pop(false);
+                    }, child: Text('No')),
+                    FlatButton(onPressed: (){
+                      Navigator.of(ctx).pop(true);
+                    }, child: Text('Yes'))
+                  ],
+                ));
+      },
+      key: ValueKey(id),
       direction: DismissDirection.endToStart,
-      onDismissed: (direction){
-          Provider.of<Cart>(context,listen: false).removeItem(productId);
+      onDismissed: (direction) {
+        Provider.of<Cart>(context, listen: false).removeItem(productId);
       },
       background: Padding(
         padding: const EdgeInsets.all(8.0),
         child: Container(
-
-          child: Icon(Icons.delete,color: Colors.white,size:40),
+          child: Icon(Icons.delete, color: Colors.white, size: 40),
           alignment: Alignment.centerRight,
-          padding: EdgeInsets.only(right:20 ),
+          padding: EdgeInsets.only(right: 20),
           decoration: BoxDecoration(
-              color:Colors.red,
-              borderRadius: BorderRadius.all(Radius.circular(5))
-          ),
+              color: Colors.red,
+              borderRadius: BorderRadius.all(Radius.circular(5))),
         ),
       ),
-
       child: Card(
         margin: EdgeInsets.symmetric(horizontal: 15, vertical: 4),
         child: Padding(
@@ -44,11 +60,16 @@ class CartItem extends StatelessWidget {
             leading: CircleAvatar(
               radius: 30.0,
               backgroundColor: kPrimaryColor[100],
-              child: FittedBox(child: Text('\$ ${price}',style: Theme.of(context).textTheme.headline4 ,)),
+              child: FittedBox(
+                  child: Text(
+                '\$ ${price}',
+                style: Theme.of(context).textTheme.headline4,
+              )),
             ),
             title: Text(title),
-            subtitle: Text('Total: \$${(price*quantity)}'),
-            trailing: Text('${quantity} x',style: Theme.of(context).textTheme.headline4),
+            subtitle: Text('Total: \$${(price * quantity)}'),
+            trailing: Text('${quantity} x',
+                style: Theme.of(context).textTheme.headline4),
           ),
         ),
       ),
