a3275cfddc0367a65d4effa30cca28444be999ab
reuben21 <reuben211999@icloud.com>
2021-05-09 17:14:57 +0530

    Adding a "Manage Products" Page

--- shop/lib/widgets/user_product_item.dart
+++ shop/lib/widgets/user_product_item.dart
@@ -10,18 +10,26 @@ class UserProductItem extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return ListTile(
+    return Container(
+      margin: EdgeInsets.all(10),
 
-      title: Text(title, style: Theme.of(context).textTheme.headline2),
-      leading: CircleAvatar(
-        backgroundImage: NetworkImage(imageUrl),
+      decoration: BoxDecoration(
+          color: kPrimaryColor[100],
+        borderRadius: BorderRadius.all(Radius.circular(10))),
+      
+      child: ListTile(
+
+        title: Text(title, style: Theme.of(context).textTheme.headline2),
+        leading: CircleAvatar(
+          backgroundImage: NetworkImage(imageUrl),
+          ),
+        trailing: Container(
+          width: 100,
+          child: Row(children: <Widget>[
+            IconButton(icon: Icon(Icons.edit), onPressed: (){},color: kSecondaryColor[100],),
+            IconButton(icon: Icon(Icons.delete), onPressed: (){},color: kSecondaryColor[100],),
+          ],),
         ),
-      trailing: Container(
-        width: 100,
-        child: Row(children: <Widget>[
-          IconButton(icon: Icon(Icons.edit), onPressed: (){},color: kSecondaryColor[100],),
-          IconButton(icon: Icon(Icons.delete), onPressed: (){},color: kSecondaryColor[100],),
-        ],),
       ),
     );
   }
