ba28997f9ac94a84e61b90c8ffbc04fc5580f44b
reuben21 <reuben211999@icloud.com>
2021-05-22 20:08:35 +0530

    Handling Authentication Errors

--- shop/lib/providers/auth.dart
+++ shop/lib/providers/auth.dart
@@ -2,6 +2,7 @@ import 'dart:convert';
 
 import 'package:http/http.dart' as http;
 import 'package:flutter/widgets.dart';
+import 'package:shop/models/http_exception.dart';
 
 class Auth with ChangeNotifier {
   String _token;
@@ -11,12 +12,23 @@ class Auth with ChangeNotifier {
   var apiKey = "AIzaSyCQffLim4mxINXHQzuEnIHaB4bFoKJN9rs";
 
   Future<void> _authenticate(String email,String password,Uri urlSegment) async {
-    final response = await http.post(urlSegment,body: json.encode({
-      'email':email,
-      'password':password,
-      'returnSecureToken':true
-    }));
-    print(json.decode(response.body));
+
+    try {
+      final response = await http.post(urlSegment,body: json.encode({
+        'email':email,
+        'password':password,
+        'returnSecureToken':true
+      }));
+      var responseData = json.decode(response.body);
+      if(responseData['error'] != null) {
+        throw HttpException(responseData['error']['message']);
+      }
+      print(json.decode(response.body));
+    }catch(error) {
+        throw error;
+    }
+
+
   }
 
   Future<void> signup(String email,String password) async {
--- shop/lib/screens/auth_screen.dart
+++ shop/lib/screens/auth_screen.dart
@@ -3,6 +3,7 @@ import 'dart:math';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:shop/colors.dart';
+import 'package:shop/models/http_exception.dart';
 import 'package:shop/providers/auth.dart';
 
 enum AuthMode { Signup, Login }
@@ -103,6 +104,17 @@ class _AuthCardState extends State<AuthCard> {
   var _isLoading = false;
   final _passwordController = TextEditingController();
 
+  void _showErrorDialog(String message) {
+    showDialog(context: context, builder: (ctx)=> AlertDialog(title: Text('An Error Occurred'),
+    content: Text(message),
+      actions: <Widget>[
+        TextButton(onPressed: (){
+          Navigator.of(ctx).pop();
+        }, child: Text('Okay'))
+      ],
+    ));
+  }
+  
   void _submit() async {
     if (!_formKey.currentState.validate()) {
       // Invalid!
@@ -112,14 +124,35 @@ class _AuthCardState extends State<AuthCard> {
     setState(() {
       _isLoading = true;
     });
-    if (_authMode == AuthMode.Login) {
-      // Log user in
-      await Provider.of<Auth>(context, listen: false)
-          .signin(_authData['email'], _authData['password']);
-    } else {
-      await Provider.of<Auth>(context, listen: false)
-          .signup(_authData['email'], _authData['password']);
+
+    try {
+      if (_authMode == AuthMode.Login) {
+        // Log user in
+        await Provider.of<Auth>(context, listen: false)
+            .signin(_authData['email'], _authData['password']);
+      } else {
+        await Provider.of<Auth>(context, listen: false)
+            .signup(_authData['email'], _authData['password']);
+      }
+    } on HttpException catch(error) {
+      var errorMessage = "Authentication Failed";
+      if(error.toString().contains('EMAIL_EXISTS')) {
+        errorMessage = 'This email id already exists';
+
+      }else if (error.toString().contains('INVALID_EMAIL')) {
+        errorMessage = 'This is not a valid Email Address';
+      } else if (error.toString().contains('WEAK_PASSWORD')) {
+        errorMessage = 'This Password is Weak';
+      } else if (error.toString().contains('EMAIL_NOT_FOUND')) {
+        errorMessage = 'Could not find this Email Id';
+      } else if (error.toString().contains('INVALID_PASSWORD')) {
+        errorMessage = 'Invalid Password.';
+      }
+      _showErrorDialog(errorMessage);
+    } catch(error) {
+      var errorMessage = 'Could Not Authenticate you,Please Try Again Later';
     }
+
     setState(() {
       _isLoading = false;
     });
