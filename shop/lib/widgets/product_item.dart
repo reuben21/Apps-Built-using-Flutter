30c000b40aa30fc9488123f3a46b3e49fcb6b360
reuben21 <reuben211999@icloud.com>
2021-05-27 11:18:24 +0530

    Adding a "Hero" Transition

--- shop/lib/widgets/product_item.dart
+++ shop/lib/widgets/product_item.dart
@@ -28,11 +28,14 @@ class ProductItem extends StatelessWidget {
                 Navigator.of(context).pushNamed(ProductDetailScreen.routeName,
                     arguments: product.id);
               },
-              child: FadeInImage(
-                placeholder:
-                    AssetImage('assets/images/product-placeholder.png'),
-                image: NetworkImage(product.imageUrl),
-                fit: BoxFit.cover,
+              child: Hero(
+                tag: product.id,
+                child: FadeInImage(
+                  placeholder:
+                      AssetImage('assets/images/product-placeholder.png'),
+                  image: NetworkImage(product.imageUrl),
+                  fit: BoxFit.cover,
+                ),
               )),
           footer: Container(
             color: kPrimaryColor[100],
