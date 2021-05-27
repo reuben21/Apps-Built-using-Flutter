ae4a8f7666e2ae756c4afa99fda6c47eebe07a4c
reuben21 <reuben211999@icloud.com>
2021-05-27 12:45:57 +0530

    Animating Order Boxes

--- lib/widgets/order_item.dart
+++ lib/widgets/order_item.dart
@@ -6,9 +6,7 @@ import 'package:intl/intl.dart';
 import 'dart:math';
 
 class OrderItem extends StatefulWidget {
-
   final ord.OrderItem order;
-
 
   OrderItem(this.order);
 
@@ -17,46 +15,66 @@ class OrderItem extends StatefulWidget {
 }
 
 class _OrderItemState extends State<OrderItem> {
-
   var _expanded = false;
-
 
   @override
   Widget build(BuildContext context) {
     // TODO: implement build
-    return Card(margin: EdgeInsets.all(10),color: kPrimaryColor[100], child: Column(
-      children: <Widget>[
-        ListTile(title: Text('\$ ${widget.order.amount.toStringAsFixed(2)}',style: Theme.of(context).textTheme.headline3),
-          subtitle: Text(
-              DateFormat('dd/MM/yyyy hh:mm').format(widget.order.dateTime)
-              ,style: Theme.of(context).textTheme.headline4
-            ),
-          trailing: IconButton(
-            icon: Icon(_expanded ? Icons.expand_less : Icons.expand_more,color: kSecondaryColor[100],),
-            onPressed: () {
-              setState(() {
-                _expanded = !_expanded;
-              });
-            },
-          ),
-        ),
-        if(_expanded) Container(
-          decoration: BoxDecoration(
-              color: kPrimaryColorAccent[100],
-              borderRadius: BorderRadius.all(Radius.circular(5))
-          ),
-          padding: EdgeInsets.all(20),
-            height: min(widget.order.products.length * 20.0 + 50.0, 180.0),
-            child: ListView( children: widget.order.products.map((prod) => Row(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
-              children: <Widget>[
-                Text(prod.title ,style: Theme.of(context).textTheme.headline4),
-                Text('${prod.quantity}x \$ ${prod.price}',style: Theme.of(context).textTheme.headline4)
-              ],
-            )).toList()),
-          )
+    return AnimatedContainer(
+      duration: Duration(milliseconds: 300),
 
-      ],
-    ));
+      child: Card(
+          margin: EdgeInsets.all(10),
+          color: kPrimaryColor[100],
+          child: Column(
+            children: <Widget>[
+              ListTile(
+                title: Text('\$ ${widget.order.amount.toStringAsFixed(2)}',
+                    style: Theme.of(context).textTheme.headline3),
+                subtitle: Text(
+                    DateFormat('dd/MM/yyyy hh:mm')
+                        .format(widget.order.dateTime),
+                    style: Theme.of(context).textTheme.headline4),
+                trailing: IconButton(
+                  icon: Icon(
+                    _expanded ? Icons.expand_less : Icons.expand_more,
+                    color: kSecondaryColor[100],
+                  ),
+                  onPressed: () {
+                    setState(() {
+                      _expanded = !_expanded;
+                    });
+                  },
+                ),
+              ),
+
+              AnimatedContainer(
+                duration: Duration(milliseconds: 300),
+                  decoration: BoxDecoration(
+                      color: kPrimaryColorAccent[100],
+                      borderRadius: BorderRadius.all(Radius.circular(5))),
+                  padding: EdgeInsets.all(20),
+                  height:_expanded ?
+                      min(widget.order.products.length * 20.0 + 50.0, 180.0) : 0,
+                  child: ListView(
+                      children: widget.order.products
+                          .map((prod) => Row(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceBetween,
+                                children: <Widget>[
+                                  Text(prod.title,
+                                      style: Theme.of(context)
+                                          .textTheme
+                                          .headline4),
+                                  Text('${prod.quantity}x \$ ${prod.price}',
+                                      style:
+                                          Theme.of(context).textTheme.headline4)
+                                ],
+                              ))
+                          .toList()),
+                )
+            ],
+          )),
+    );
   }
 }
