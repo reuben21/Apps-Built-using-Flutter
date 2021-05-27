64e55e0b67e6ab81b675ac15613013726b45b5c4
reuben21 <reuben211999@icloud.com>
2021-05-27 12:37:55 +0530

    Working with Slivers

--- shop/lib/screens/product_detail_screen.dart
+++ shop/lib/screens/product_detail_screen.dart
@@ -19,46 +19,67 @@ class ProductDetailScreen extends StatelessWidget {
         Provider.of<Products>(context, listen: false).findById(productId);
 
     return Scaffold(
-      appBar: AppBar(
-        title: Text(loadedProduct.title,
-            style: Theme.of(context).textTheme.headline1),
-      ),
+      // appBar: AppBar(
+      //   title: Text(loadedProduct.title,
+      //       style: Theme.of(context).textTheme.headline1),
+      // ),
       body: Container(
         height: double.infinity,
         color: kPrimaryColorAccent[100],
-        child: Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: SingleChildScrollView(
-            child: Column(
-              children: [
-                Container(
-                  height: 300,
-                  width: double.infinity,
-                  child:
-                      Hero(tag: loadedProduct.id,
-                          child: Image.network(loadedProduct.imageUrl, fit: BoxFit.cover)),
-                  decoration: BoxDecoration(
-                      borderRadius: BorderRadius.all(Radius.circular(10))),
+        child: CustomScrollView(
+            slivers: <Widget>[
+              SliverAppBar(
+                expandedHeight: 300,
+                pinned: true,
+                flexibleSpace: FlexibleSpaceBar(
+
+                  background: Container(
+                    height: 300,
+                    width: double.infinity,
+                    child: Hero(
+                        tag: loadedProduct.id,
+                        child: Image.network(loadedProduct.imageUrl,
+                            fit: BoxFit.cover)),
+                    decoration: BoxDecoration(
+                        borderRadius: BorderRadius.all(Radius.circular(10))),
+                  ),
                 ),
+              ),
+              SliverList(
+
+                  delegate: SliverChildListDelegate([
+                    Text(
+                      "${loadedProduct.title}",
+
+                      style: Theme.of(context).textTheme.headline1,
+                      textAlign: TextAlign.center,
+                    ),
                 SizedBox(
                   height: 10,
                 ),
                 Text(
-                  "Price \$ ${loadedProduct.price}",
+                  "Price : \$ ${loadedProduct.price}",
                   style: Theme.of(context).textTheme.headline2,
+                  textAlign: TextAlign.center,
                 ),
+                Divider(color: kSecondaryColor[100],),
                 SizedBox(
                   height: 10,
                 ),
-                Text(
-                  loadedProduct.description,
-                  style: Theme.of(context).textTheme.headline4,
-                )
-              ],
-            ),
+                Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Text(
+                    loadedProduct.description,
+                    style: Theme.of(context).textTheme.headline4,
+                  ),
+                ),
+                SizedBox(
+                  height: 800,
+                ),
+              ]))
+            ],
           ),
         ),
-      ),
     );
   }
 }
